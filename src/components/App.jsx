be0d8ed445df43ac2d6600d400959926725142cf
dmkomur <125804681+dmkomur@users.noreply.github.com>
2023-05-24 18:22:09 +0300

    update to hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 
 import { FormAdd } from './FormAdd/FormAdd';
@@ -11,49 +11,84 @@ const SAMPLE = [
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
-export class App extends React.Component {
-  state = {
-    contacts: SAMPLE,
-    filters: '',
-  };
 
-  onFormSubmit = data => {
+export const App = () => {
+  const [contacts, setContacts] = useState(SAMPLE);
+
+  const [filters, setFilters] = useState('');
+
+  const onFormSubmit = (name, number) => {
     const newContact = {
       id: nanoid(),
-      name: data.name,
-      number: data.number,
+      name,
+      number,
     };
-
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+    setContacts(prevState => [...prevState, newContact]);
   };
 
-  handleDelete = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(el => el.id !== id),
-    }));
+  const handleDelete = id => {
+    setContacts(prevState => prevState.filter(el => el.id !== id));
   };
-  handleSearch = event => {
-    this.setState({
-      filters: event.target.value,
-    });
+  const handleSearch = event => {
+    setFilters(event.target.value);
   };
-  render() {
-    const filteredContacts = this.state.contacts.filter(el =>
-      el.name.toLowerCase().includes(this.state.filters.toLocaleLowerCase())
-    );
 
-    return (
-      <div className="container">
-        <h1>Phonebook</h1>
-        <FormAdd
-          handleForm={this.onFormSubmit}
-          contacts={this.state.contacts}
-        />
-        <Search onSearch={this.handleSearch} />
-        <ContactList fileList={filteredContacts} onDelete={this.handleDelete} />
-      </div>
-    );
-  }
-}
+  const filteredContacts = contacts.filter(el =>
+    el.name.toLowerCase().includes(filters.toLocaleLowerCase())
+  );
+
+  return (
+    <div className="container">
+      <h1>Phonebook</h1>
+      <FormAdd handleForm={onFormSubmit} contacts={contacts} />
+      <Search onSearch={handleSearch} />
+      <ContactList fileList={filteredContacts} onDelete={handleDelete} />
+    </div>
+  );
+};
+// export class App extends React.Component {
+//   state = {
+//     contacts: SAMPLE,
+//     filters: '',
+//   };
+
+//   onFormSubmit = (name, number) => {
+//     const newContact = {
+//       id: nanoid(),
+//       name,
+//       number,
+//     };
+
+//     this.setState(prevState => ({
+//       contacts: [...prevState.contacts, newContact],
+//     }));
+//   };
+
+//   handleDelete = id => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(el => el.id !== id),
+//     }));
+//   };
+//   handleSearch = event => {
+//     this.setState({
+//       filters: event.target.value,
+//     });
+//   };
+//   render() {
+//     const filteredContacts = this.state.contacts.filter(el =>
+//       el.name.toLowerCase().includes(this.state.filters.toLocaleLowerCase())
+//     );
+
+//     return (
+//       <div className="container">
+//         <h1>Phonebook</h1>
+//         <FormAdd
+//           handleForm={this.onFormSubmit}
+//           contacts={this.state.contacts}
+//         />
+//         <Search onSearch={this.handleSearch} />
+//         <ContactList fileList={filteredContacts} onDelete={this.handleDelete} />
+//       </div>
+//     );
+//   }
+// }
